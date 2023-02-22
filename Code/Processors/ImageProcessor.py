97ab97351aeaf52a2d726a28b7ba728b47818e02
Ali Jafari <alijfri99@gmail.com>
2023-02-22 18:11:30 +0330

    Changed JPGProcessor to ImageProcessor.

--- Code/Processors/ImageProcessor.py
+++ Code/Processors/ImageProcessor.py
@@ -2,7 +2,7 @@ import numpy
 from PIL import Image
 from Processors.Processor import Processor
 
-class JPGProcessor(Processor):
+class ImageProcessor(Processor):
     def process(self, path):
         image = Image.open(path)
         image = numpy.asarray(image)
