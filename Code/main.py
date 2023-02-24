1b8f088f3f2145715af38b187b4e0f869f689737
Ali Jafari <alijfri99@gmail.com>
2023-02-24 14:35:45 +0330

    The project is now stable.

--- Code/main.py
+++ Code/main.py
@@ -3,7 +3,7 @@ from Processors.ImageProcessor import ImageProcessor
 import matplotlib.pyplot as plt
 
 a = LabeledDatasetExtractor(ImageProcessor(), 60000, (28, 28))
-a.extract("/home/ali/Downloads/MNIST-JPG-master/MNIST Dataset JPG format/MNIST - JPG - training")
+a.extract("/home/alijfri99/Projects/Unsupervised-Domain-Adaptation/Data/MNIST Dataset JPG format/MNIST - JPG - testing")
 while True:
     inp = int(input("Enter an index: "))
     plt.imshow(a.data[inp], cmap='gray')
