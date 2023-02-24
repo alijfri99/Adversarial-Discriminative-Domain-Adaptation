bb3d142133359001cd21557aa2ef63d001b2c88a
Ali Jafari <alijfri99@gmail.com>
2023-02-24 17:34:39 +0330

    Implemented transform for LabeledDataset.

--- Code/Datasets/LabeledDataset.py
+++ Code/Datasets/LabeledDataset.py
@@ -4,7 +4,7 @@ import os
 from torch.utils.data import Dataset
 
 class LabeledDataset(Dataset):
-    def __init__(self, dataset_path, dataset_name):
+    def __init__(self, dataset_path, dataset_name, transform=None):
         dataset_store_location = os.path.join(dataset_path, dataset_name) + ".npz"
         class_dict_store_location = os.path.join(dataset_path, dataset_name) + "_class_dict.pkl"
 
@@ -15,8 +15,16 @@ class LabeledDataset(Dataset):
         with open(class_dict_store_location, 'rb') as input_file:
             self.class_dict = pickle.load(input_file)
 
+        self.transform = transform
+
     def __len__(self):
         return self.data.shape[0]
     
     def __getitem__(self, index):
-        return self.data[index], self.labels[index]
+        item_data = self.data[index]
+        item_label = self.labels[index]
+        
+        if self.transform:
+            item_data = self.transform(item_data)
+        
+        return item_data, item_label
--- Code/main.py
+++ Code/main.py
@@ -1,12 +1,16 @@
 from Extractors.LabeledDatasetExtractor import LabeledDatasetExtractor
 from Processors.ImageProcessor import ImageProcessor
 from Datasets.LabeledDataset import LabeledDataset
+from torchvision import transforms
 import matplotlib.pyplot as plt
 
-a = LabeledDataset("StoredDatasets/MNIST", "training")
+a = LabeledDataset("StoredDatasets/MNIST", "training", transforms.ToTensor())
 print("DONE!", len(a))
 while True:
     inp = int(input("Enter an index: "))
+    b = a.__getitem__(inp)
+    print(b, type(b))
+    input()
     plt.imshow(a.data[inp], cmap='gray')
     print(a.labels[inp])
     print(a.class_dict[a.labels[inp]])
