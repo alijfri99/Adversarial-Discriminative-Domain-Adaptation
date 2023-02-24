c95291252d634c2a162659d3ebecbaa4767f4132
Ali Jafari <alijfri99@gmail.com>
2023-02-24 16:22:40 +0330

    Tested LabeledDataset.

--- Code/main.py
+++ Code/main.py
@@ -4,10 +4,10 @@ from Datasets.LabeledDataset import LabeledDataset
 import matplotlib.pyplot as plt
 
 a = LabeledDataset("StoredDatasets/MNIST", "training")
-print("DONE!")
+print("DONE!", len(a))
 while True:
     inp = int(input("Enter an index: "))
     plt.imshow(a.data[inp], cmap='gray')
     print(a.labels[inp])
-    print(a.class_dict[inp])
+    print(a.class_dict[a.labels[inp]])
     plt.show()
