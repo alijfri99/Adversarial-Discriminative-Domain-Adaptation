c4b2363f06ba4f4d6d980415782cf6c83c623f98
Ali Jafari <alijfri99@gmail.com>
2023-02-24 23:15:37 +0330

    Tested some transforms.

--- Code/main.py
+++ Code/main.py
@@ -4,14 +4,13 @@ from Datasets.LabeledDataset import LabeledDataset
 from torchvision import transforms
 import matplotlib.pyplot as plt
 
-a = LabeledDataset("StoredDatasets/MNIST", "training", transforms.ToTensor())
+a = LabeledDataset("StoredDatasets/MNIST", "training", transforms.Compose([transforms.ToTensor(), transforms.RandomRotation(90)]))
 print("DONE!", len(a))
 while True:
     inp = int(input("Enter an index: "))
     b = a.__getitem__(inp)
     print(b, type(b))
-    input()
-    plt.imshow(a.data[inp], cmap='gray')
+    plt.imshow(a.__getitem__(inp)[0].numpy()[0], cmap='gray')
     print(a.labels[inp])
     print(a.class_dict[a.labels[inp]])
     plt.show()
