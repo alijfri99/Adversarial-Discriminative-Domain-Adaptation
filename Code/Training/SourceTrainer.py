bf0decadfdbb7e8626d9d3457aafb603709cb3b1
behrouz.ghamkhar <behrouz.ghamkhar@partsoftware.com>
2023-03-10 17:04:26 +0330

    Added optimizer to constructor parameters and Changed method name to train

--- Code/Training/SourceTrainer.py
+++ Code/Training/SourceTrainer.py
@@ -3,32 +3,30 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 class SourceTrainer:
-    def __init__(self, source_encoder, classifier,  source_dataset, criterion,
-                     num_epochs, batch_size, learning_rate, device, shuffle=True):
+    def __init__(self, source_encoder, classifier,  source_dataset, criterion, optimizer,
+                     num_epochs, batch_size, device, shuffle=True):
         self.source_encoder = source_encoder
         self.classifier = classifier
         self.source_dataset = source_dataset
         self.criterion = criterion
+        self.optimizer = optimizer
         self.num_epochs = num_epochs
         self.batch_size = batch_size
-        self.learning_rate = learning_rate
         self.device = device
         self.shuffle = shuffle
         
-    def train_source(self):
+    def train(self):
         train_loader = DataLoader(self.source_dataset, self.batch_size, self.shuffle)
-        
-        optimizer = optim.Adam(list(self.source_encoder.parameters()) + list(self.classifier.parameters()), lr=self.learning_rate)
 
         for epoch in range(self.num_epochs):
              for index, (data, labels) in enumerate(train_loader):
                 data = data.to(self.device)
                 labels = labels.to(self.device)
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
 
                 features = self.source_encoder(data)
                 predictions = self.classifier(features)
                 loss = self.criterion(predictions, labels)
 
                 loss.backward()
-                optimizer.step()
+                self.optimizer.step()
