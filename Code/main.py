7c2136c054229753812e632e1020ce0564664582
behrouz.ghamkhar <behrouz.ghamkhar@partsoftware.com>
2023-03-14 16:36:24 +0330

    Wrote a test for source trainer and network tester in main

--- Code/main.py
+++ Code/main.py
@@ -5,16 +5,32 @@ from torchvision import transforms
 from Networks.Encoders.LeNetEncoder import LeNetEncoder
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
+from Training.SourceTrainer import SourceTrainer
+from Training.NetworkTester import NetworkTester
+import torch
+import torch.nn as nn
+import torchvision
 
-b = LabeledDataset('StoredDatasets/MNIST', 'training', transforms.Compose([transforms.ToTensor()]))
-print("dataset size:", b.__len__(), b.data.shape, b.labels.shape)
 
-loader = DataLoader(b, 4)
+labeledDataset = LabeledDataset('Code/StoredDatasets/MNIST', 'training', transforms.Compose([transforms.ToTensor()]))
+print("dataset size:", labeledDataset.__len__(), labeledDataset.data.shape, labeledDataset.labels.shape)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cpu')
 
-model = LeNetEncoder()
+num_epoch = 2
+batch_size = 100
+learning_rate = 0.001
 
-for data, label in loader:
-    print(data.shape)
-    pred = model(data)
-    print(pred, pred.shape)
-    input()
+loader = DataLoader(labeledDataset, 4)
+encoder = LeNetEncoder()
+
+classifier = nn.Linear(500, 10)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(list(encoder.parameters()) + list(classifier.parameters()), lr = learning_rate)
+
+sourceTrainer = SourceTrainer(encoder, classifier, labeledDataset, criterion, optimizer, num_epoch, batch_size, device, True)
+sourceTrainer.train()
+
+netTester = NetworkTester(encoder, classifier, labeledDataset, batch_size, device)
+netTester.test()
+
