fa1d7801f021c6cbb6d06f4ffe43d182fb6bab78
Ali Jafari <alijfri99@gmail.com>
2023-03-18 11:24:53 +0330

    Copied network weights correctly with load_state_dict().

--- Code/Training/Adapter.py
+++ Code/Training/Adapter.py
@@ -4,9 +4,10 @@ from Training.NetworkTester import NetworkTester
 import torch.nn as nn
 
 class Adapter:
-    def __init__(self, source_encoder, source_dataset, target_dataset, criterion, optimizer, num_epochs, batch_size, device):
+    def __init__(self, classifier, source_encoder, target_encoder, source_dataset, target_dataset, criterion, optimizer, num_epochs, batch_size, device):
         self.source_encoder = source_encoder
-        self.target_encoder = copy.deepcopy(self.source_encoder)
+        self.target_encoder = target_encoder
+        self.target_encoder.load_state_dict(source_encoder.state_dict())
         self.source_dataset = source_dataset
         self.target_dataset = target_dataset
         self.criterion = criterion
@@ -14,9 +15,16 @@ class Adapter:
         self.num_epochs = num_epochs
         self.batch_size = batch_size
         self.device = device
-        self.classifier = nn.Linear(500, 10)
+        self.classifier = classifier
 
     def adapt(self):
         source_trainer = SourceTrainer(self.target_encoder, self.classifier, self.target_dataset, self.criterion, self.optimizer, self.num_epochs, self.batch_size, self.device)
         source_trainer.train()
-        tester1 = NetworkTester(self.target_encoder, self.classifier, self)
+        tester1 = NetworkTester(self.target_encoder, self.classifier, self.target_dataset, 4, 'cpu')
+        tester1.test()
+        tester2 = NetworkTester(self.target_encoder, self.classifier, self.source_dataset, 4, 'cpu')
+        tester2.test()
+        tester3 = NetworkTester(self.source_encoder, self.classifier, self.target_dataset, 4, 'cpu')
+        tester3.test()
+        tester4 = NetworkTester(self.source_encoder, self.classifier, self.source_dataset, 4, 'cpu')
+        tester4.test()
--- Code/main.py
+++ Code/main.py
@@ -7,14 +7,23 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from Training.SourceTrainer import SourceTrainer
 from Training.NetworkTester import NetworkTester
+from Training.Adapter import Adapter
 import torch
 import torch.nn as nn
 import torchvision
 
 
+mnist = LabeledDataset('Code/StoredDatasets/MNIST', 'mnist_training', transforms.Compose([transforms.ToTensor()]), sample_size=2000)
+usps = LabeledDataset('Code/StoredDatasets/USPS', 'usps_training', transforms.Compose([transforms.ToTensor()]), sample_size=1800)
+source_encoder = LeNetEncoder()
+target_encoder = LeNetEncoder()
+classifier = nn.Linear(500, 10)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(list(target_encoder.parameters()) + list(classifier.parameters()), lr = 0.001)
+adaper = Adapter(classifier, source_encoder, target_encoder, mnist, usps, criterion, optimizer, 5, 4, 'cpu')
+adaper.adapt()
 
-
-
+'''
 #labeledDataset = LabeledDataset('Code/StoredDatasets/MNIST', 'training', transforms.Compose([transforms.ToTensor()]))
 labeledDataset = LabeledDataset('Code/StoredDatasets/USPS', 'usps_training', transforms.Compose([transforms.ToTensor()]), sample_size=1800)
 print("dataset size:", labeledDataset.__len__(), labeledDataset.data.shape, labeledDataset.labels.shape)
@@ -37,21 +46,22 @@ sourceTrainer.train()
 
 netTester = NetworkTester(encoder, classifier, labeledDataset, batch_size, device)
 netTester.test()
-
+'''
 
 
 '''
 # USPS Dataset Extractor (28, 28)
 target_size = (28, 28)
-training_dataset_size = 7291 
-testing_dataset_size = 2007 
+training_dataset_size = 60000 
+testing_dataset_size = 10000
 
 imageProcessor = ImageProcessor()
 training_dataset_extractor = LabeledDatasetExtractor(imageProcessor, training_dataset_size, target_size)
-training_dataset_extractor.extract('Data/USPS/training')
-training_dataset_extractor.save('Code/StoredDatasets/USPS', 'usps_training')
+training_dataset_extractor.extract('Data/MNIST/training')
+training_dataset_extractor.save('Code/StoredDatasets/MNIST', 'mnist_training')
 
 testing_dataset_extractor = LabeledDatasetExtractor(imageProcessor, testing_dataset_size, target_size)
-testing_dataset_extractor.extract('Data/USPS/testing')
-testing_dataset_extractor.save('Code/StoredDatasets/USPS', 'usps_testing')
+testing_dataset_extractor.extract('Data/MNIST/testing')
+testing_dataset_extractor.save('Code/StoredDatasets/MNIST', 'mnist_testing')
 '''
+
