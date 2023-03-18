8d7ce17ad4bd4014831dc220f699f8750e632ad6
Ali Jafari <alijfri99@gmail.com>
2023-03-18 13:16:17 +0330

    Implememted LeNetDiscriminator.

---
+++ Code/Networks/Discriminators/LeNetDiscriminator.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class LeNetDiscriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(500, 500)
+        self.linear2 = nn.Linear(500, 500)
+        self.linear3 = nn.Linear(500, 1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        x = self.relu(x)
+        x = self.linear3(x)
+        x = self.sigmoid(x)
+        return x
