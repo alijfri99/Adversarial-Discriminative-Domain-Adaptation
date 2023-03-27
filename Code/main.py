13f507ad5bc9e5151b09ed30a6df95b864f94e4a
behrouz.ghamkhar <behrouz.ghamkhar@partsoftware.com>
2023-03-27 18:11:17 +0430

    Resolved merge conflict

--- Code/main.py
+++ Code/main.py
@@ -27,8 +27,11 @@ while True:
     plt.imshow(svhn[inp][0][0], cmap='gray')
     plt.show()'''
 
-svhn = LabeledDataset('Code/StoredDatasets/SVHN', 'svhn_training', transforms.Compose([transforms.ToTensor()]))
-mnist = LabeledDataset('Code/StoredDatasets/MNIST', 'mnist_training', transforms.Compose([transforms.ToTensor()]))
+#svhn = LabeledDataset('Code/StoredDatasets/SVHN', 'svhn_training', transforms.Compose([transforms.ToTensor()]))
+#mnist = LabeledDataset('Code/StoredDatasets/MNIST', 'mnist_training', transforms.Compose([transforms.ToTensor()]))
+
+mnist = LabeledDataset('Code/StoredDatasets/MNIST', 'training', transforms.Compose([transforms.ToTensor()]), sample_size=2000)
+usps = LabeledDataset('Code/StoredDatasets/USPS', 'usps_training', transforms.Compose([transforms.ToTensor()]), sample_size=1800)
 
 source_encoder = LeNetEncoder()
 target_encoder = LeNetEncoder()
@@ -38,20 +41,9 @@ batch_size = 128
 num_iterations = 10000
 classification_criterion = nn.CrossEntropyLoss()
 
-classification_optimizer = torch.optim.Adam(list(source_encoder.parameters()) + list(classifier.parameters()), lr=0.001)
-source_trainer = SourceTrainer(source_encoder, classifier, svhn, classification_criterion, classification_optimizer, 10, batch_size, 'cpu')
-source_trainer.train()
-source_tester = NetworkTester(source_encoder, classifier, svhn, batch_size, 'cpu')
-source_tester.test()
+classification_lr = 0.001
+discriminator_lr = target_encoder_lr = 0.0002
 
-target_encoder.load_state_dict(source_encoder.state_dict())
-
-target_tester = NetworkTester(target_encoder, classifier, mnist, batch_size, 'cpu')
-target_tester.test()
-
-discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-target_encoder_optimizer = torch.optim.Adam(target_encoder.parameters(), lr=0.0002, betas=(0.5, 0.999))
-
-adapter = Adapter(source_encoder, target_encoder, discriminator, svhn, mnist, discriminator_optimizer, target_encoder_optimizer, 10000, 128, 'cpu')
-adapter.adapt()
-target_tester.test()
+experiment = Experiment(mnist, usps, source_encoder, target_encoder, classifier, discriminator, batch_size, classification_criterion,
+                        classification_lr, discriminator_lr, target_encoder_lr, num_iterations, 'cpu')
+experiment.run()
