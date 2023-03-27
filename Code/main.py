44110d6fcbba04c5316da92fc46af0dbe34216f0
behrouz.ghamkhar <behrouz.ghamkhar@partsoftware.com>
2023-03-27 18:02:47 +0430

    Wrote a test for Experiment class

--- Code/main.py
+++ Code/main.py
@@ -6,15 +6,13 @@ from Networks.Encoders.LeNetEncoder import LeNetEncoder
 from Networks.Discriminators.LeNetDiscriminator import LeNetDiscriminator
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
-from Training.SourceTrainer import SourceTrainer
-from Training.NetworkTester import NetworkTester
-from Training.Adapter import Adapter
+from Experiments.Experiment import Experiment
 import torch
 import torch.nn as nn
 import torchvision
 
 
-mnist = LabeledDataset('Code/StoredDatasets/MNIST', 'mnist_training', transforms.Compose([transforms.ToTensor()]), sample_size=2000)
+mnist = LabeledDataset('Code/StoredDatasets/MNIST', 'training', transforms.Compose([transforms.ToTensor()]), sample_size=2000)
 usps = LabeledDataset('Code/StoredDatasets/USPS', 'usps_training', transforms.Compose([transforms.ToTensor()]), sample_size=1800)
 
 source_encoder = LeNetEncoder()
@@ -22,21 +20,12 @@ target_encoder = LeNetEncoder()
 classifier = nn.Linear(500, 10)
 discriminator = LeNetDiscriminator()
 batch_size = 128
-
+num_iterations = 10000
 classification_criterion = nn.CrossEntropyLoss()
 
-classification_optimizer = torch.optim.Adam(list(source_encoder.parameters()) + list(classifier.parameters()), lr=0.001)
-source_trainer = SourceTrainer(source_encoder, classifier, mnist, classification_criterion, classification_optimizer, 10, batch_size, 'cpu')
-source_trainer.train()
-source_tester = NetworkTester(source_encoder, classifier, mnist, batch_size, 'cpu')
-source_tester.test()
+classification_lr = 0.001
+discriminator_lr = target_encoder_lr = 0.0002
 
-target_encoder.load_state_dict(source_encoder.state_dict())
-
-discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-target_encoder_optimizer = torch.optim.Adam(target_encoder.parameters(), lr=0.0002, betas=(0.5, 0.999))
-
-adapter = Adapter(source_encoder, target_encoder, discriminator, mnist, usps, discriminator_optimizer, target_encoder_optimizer, 10000, batch_size, 'cpu')
-adapter.adapt()
-target_tester = NetworkTester(target_encoder, classifier, usps, batch_size, 'cpu')
-target_tester.test()
+experiment = Experiment(mnist, usps, source_encoder, target_encoder, classifier, discriminator, batch_size, classification_criterion,
+                        classification_lr, discriminator_lr, target_encoder_lr, num_iterations, 'cpu')
+experiment.run()
