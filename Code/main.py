dd0792716344c1aa28f78b68e0194feb2953b727
Ali Jafari <alijfri99@gmail.com>
2023-03-28 14:15:15 +0330

    Prepared the modality adaptation experiment.

--- Code/main.py
+++ Code/main.py
@@ -10,6 +10,7 @@ from Experiments.Experiment import Experiment
 from Networks.Encoders.Identity import Identity
 from Networks.Encoders.VGG16Encoder import VGG16Encoder
 from Networks.Classifiers.VGG16Classifier import VGG16Classifier
+from Networks.Discriminators.VGG16Discriminator import VGG16Discriminator
 import torch
 import torch.nn as nn
 import torchvision
@@ -25,38 +26,29 @@ extractor = LabeledDatasetExtractor(ImageProcessor(), 10000, (28, 28))
 extractor.extract('Data/MNIST/testing')
 extractor.save('Code/StoredDatasets/MNIST', 'mnist_testing')'''
 
-'''svhn = LabeledDataset('Code/StoredDatasets/SVHN', 'svhn_training', transforms.Compose([transforms.ToTensor()]))
-while True:
-    inp = int(input("Enter: "))
-    plt.imshow(svhn[inp][0][0], cmap='gray')
-    plt.show()'''
+device = ('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Device: {device}')
 
-'''svhn = LabeledDataset('Code/StoredDatasets/NYUD2', 'nyud2_training', transforms.Compose([transforms.ToTensor()]))
-mnist = LabeledDataset('Code/StoredDatasets/MNIST', 'mnist_training', transforms.Compose([transforms.ToTensor()]))
-print(svhn.data.shape)
-input()
+nyurgb = LabeledDataset('/content/drive/MyDrive/StoredDatasets/NYUD2', 'nyud2_training', transforms.ToTensor())
+nyudepth = LabeledDataset('/content/drive/MyDrive/StoredDatasets/NYUD2', 'nyud2_testing', transforms.ToTensor())
+print(numpy.unique(nyurgb.labels))
+print(numpy.min(nyurgb.labels))
+print(numpy.max(nyurgb.labels))
 
-source_encoder = LeNetEncoder()
-target_encoder = LeNetEncoder()
-classifier = nn.Linear(500, 10)
-discriminator = LeNetDiscriminator()
+source_encoder = VGG16Encoder().to(device)
+target_encoder = VGG16Encoder().to(device)
+classifier = VGG16Classifier().to(device)
+discriminator = VGG16Discriminator().to(device)
 batch_size = 128
-num_iterations = 10000
+num_iterations = 20000
 classification_criterion = nn.CrossEntropyLoss()
+epochs = 10
 
 classification_lr = 0.001
 discriminator_lr = target_encoder_lr = 0.0002
 
-experiment = Experiment(svhn, mnist, source_encoder, target_encoder, classifier, discriminator, batch_size, classification_criterion,
-                        classification_lr, discriminator_lr, target_encoder_lr, num_iterations, 'cpu')
-experiment.run()'''
-vgg = VGG16Encoder()
-classifier = VGG16Classifier()
-print(vgg)
-print(classifier)
+print("Starting the experiment")
 
-inp = torch.randn((3, 224, 224))
-out = vgg(inp)
-print(out, out.shape)
-out2 = classifier(out)
-print(out2, out2.shape)
+experiment = Experiment(nyurgb, nyudepth, source_encoder, target_encoder, classifier, discriminator, epochs, batch_size, classification_criterion,
+                        classification_lr, discriminator_lr, target_encoder_lr, num_iterations, device)
+experiment.run()
