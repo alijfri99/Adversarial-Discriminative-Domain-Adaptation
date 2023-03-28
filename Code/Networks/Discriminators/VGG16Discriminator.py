d2499882b1d478f50dc59e1e6274c60ade56415e
Ali Jafari <alijfri99@gmail.com>
2023-03-28 12:30:12 +0330

    Implemented VGG16Discriminator.

---
+++ Code/Networks/Discriminators/VGG16Discriminator.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class VGG16Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(25088, 1024)
+        self.linear2 = nn.Linear(1024, 2048)
+        self.linear3 = nn.Linear(2048, 1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        x = self.relu(x)
+        x = self.linear3(x)
+        x = self.sigmoid(x)
+        return x
