3ddef2cc1249e285476c54166642458c82a6ff3a
Ali Jafari <alijfri99@gmail.com>
2023-03-29 14:03:51 +0330

    Implemented the SVHN --> MNIST separately.

---
+++ Code/svhn_mnist.py
@@ -0,0 +1,31 @@
+import torch
+from Datasets.LabeledDataset import LabeledDataset
+from torchvision import transforms
+from Networks.Encoders.LeNetEncoder import LeNetEncoder
+from Networks.Discriminators.DigitsDiscriminator import DigitsDiscriminator
+from Experiments.Experiment import Experiment
+
+device = ('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Device: {device}')
+
+svhn = LabeledDataset('Code/StoredDatasets/SVHN', 'svhn_training', transforms.ToTensor())
+mnist = LabeledDataset('Code/StoredDatasets/MNIST', 'mnist_training', transforms.ToTensor())
+
+source_encoder = LeNetEncoder().to(device)
+target_encoder = LeNetEncoder().to(device)
+classifier = torch.nn.Linear(500, 10).to(device)
+discriminator = DigitsDiscriminator().to(device)
+classification_epochs = 10
+batch_size = 128
+adaptation_iterations = 10000
+classification_criterion = torch.nn.CrossEntropyLoss()
+
+classification_optimizer = torch.optim.Adam(list(source_encoder.parameters()) + list(classifier.parameters()), lr=0.001)
+discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+target_encoder_optimizer = torch.optim.Adam(target_encoder.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+print('Starting the SVHN --> MNIST experiment.')
+
+experiment = Experiment(svhn, mnist, source_encoder, target_encoder, classifier, discriminator, classification_epochs, batch_size, classification_criterion,
+                        classification_optimizer, discriminator_optimizer, target_encoder_optimizer, adaptation_iterations, device)
+experiment.run()
