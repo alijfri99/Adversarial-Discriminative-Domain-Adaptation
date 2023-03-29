ceb204e3d651f0e57012dbb50a2559808664fc98
Ali Jafari <alijfri99@gmail.com>
2023-03-29 13:21:58 +0330

    Updated the Experiment class.

--- Code/Experiments/Experiment.py
+++ Code/Experiments/Experiment.py
@@ -5,36 +5,32 @@ from Training.Adapter import Adapter
 
 
 class Experiment:
-    def __init__(self, source_dataset, target_dataset, source_encoder, target_encoder, classifier, discriminator, epochs, batch_size, classification_criterion,
-                    classification_lr, adaptation_lr, num_iterations, device):
+    def __init__(self, source_dataset, target_dataset, source_encoder, target_encoder, classifier, discriminator, classification_epochs, batch_size, classification_criterion,
+                    classification_optimizer, discriminator_optimizer, target_encoder_optimizer, adaptation_iterations, device):
         self.source_dataset = source_dataset
         self.target_dataset = target_dataset
         self.source_encoder = source_encoder
         self.target_encoder = target_encoder
         self.classifier = classifier
         self.discriminator = discriminator
-        self.epochs = epochs
+        self.classification_epochs = classification_epochs
         self.batch_size = batch_size
         self.classification_criterion = classification_criterion
-        self.classification_lr = classification_lr
-        self.adaptation_lr = adaptation_lr
-        self.num_iterations = num_iterations
+        self.classification_optimizer = classification_optimizer
+        self.discriminator_optimizer = discriminator_optimizer
+        self.target_encoder_optimizer = target_encoder_optimizer
+        self.adaptation_iterations = adaptation_iterations
         self.device = device
 
-
     def run(self):
-        classification_optimizer = torch.optim.Adam(list(self.source_encoder.parameters()) + list(self.classifier.parameters()), lr=self.classification_lr)
-        source_trainer = SourceTrainer(self.source_encoder, self.classifier, self.source_dataset, self.classification_criterion, classification_optimizer, self.epochs, self.batch_size, self.device)
+        source_trainer = SourceTrainer(self.source_encoder, self.classifier, self.source_dataset, self.classification_criterion, self.classification_optimizer, self.classification_epochs, self.batch_size, self.device)
         source_trainer.train()
         source_tester = NetworkTester(self.source_encoder, self.classifier, self.source_dataset, self.batch_size, self.device)
         source_tester.test()
         self.target_encoder.load_state_dict(self.source_encoder.state_dict())
 
-        discriminator_optimizer = torch.optim.Adam(self.discriminator.parameters(), lr=self.adaptation_lr, betas=(0.5, 0.999))
-        target_encoder_optimizer = torch.optim.Adam(self.target_encoder.parameters(), lr=self.adaptation_lr, betas=(0.5, 0.999))
-
         adapter = Adapter(self.source_encoder, self.target_encoder, self.discriminator, self.classifier, self.source_dataset, self.target_dataset,
-                             discriminator_optimizer, target_encoder_optimizer, self.num_iterations, self.batch_size, self.device)
+                             self.discriminator_optimizer, self.target_encoder_optimizer, self.adaptation_iterations, self.batch_size, self.device)
         adapter.adapt()
         target_tester = NetworkTester(self.target_encoder, self.classifier, self.target_dataset, self.batch_size, self.device)
         target_tester.test()
--- Code/mnist_usps.py
+++ Code/mnist_usps.py
@@ -16,16 +16,17 @@ source_encoder = LeNetEncoder().to(device)
 target_encoder = LeNetEncoder().to(device)
 classifier = nn.Linear(500, 10).to(device)
 discriminator = DigitsDiscriminator().to(device)
+classification_epochs = 10
 batch_size = 128
-num_iterations = 10000
+adaptation_iterations = 10000
 classification_criterion = nn.CrossEntropyLoss()
-epochs = 10
 
-classification_lr = 0.001
-adaptation_lr = 0.0002
+classification_optimizer = torch.optim.Adam(list(source_encoder.parameters()) + list(classifier.parameters()), lr=0.001)
+discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+target_encoder_optimizer = torch.optim.Adam(target_encoder.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
 print('Starting the MNIST --> USPS experiment.')
 
-experiment = Experiment(mnist, usps, source_encoder, target_encoder, classifier, discriminator, epochs, batch_size, classification_criterion,
-                        classification_lr, adaptation_lr, num_iterations, device)
+experiment = Experiment(mnist, usps, source_encoder, target_encoder, classifier, discriminator, classification_epochs, batch_size, classification_criterion,
+                        classification_optimizer, discriminator_optimizer, target_encoder_optimizer, adaptation_iterations, device)
 experiment.run()
