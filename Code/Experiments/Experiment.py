1222cdc21314ab4927cde067028523bdf40dd38a
Ali Jafari <alijfri99@gmail.com>
2023-03-29 14:47:27 +0330

    Fixed outputs in Experiment.

--- Code/Experiments/Experiment.py
+++ Code/Experiments/Experiment.py
@@ -25,11 +25,15 @@ class Experiment:
         source_trainer = SourceTrainer(self.source_encoder, self.classifier, self.source_dataset, self.classification_criterion, self.classification_optimizer, self.num_iterations, self.batch_size, self.device)
         source_trainer.train()
         source_tester = NetworkTester(self.source_encoder, self.classifier, self.source_dataset, self.batch_size, self.device)
+        print('Testing the accuracy of source encoder on the source domain...')
         source_tester.test()
         self.target_encoder.load_state_dict(self.source_encoder.state_dict())
+        target_tester = NetworkTester(self.target_encoder, self.classifier, self.target_dataset, self.batch_size, self.device)
+        print('Testing the accuracy of target encoder on the target domain without adaptation (source only)...')
+        target_tester.test()
 
         adapter = Adapter(self.source_encoder, self.target_encoder, self.discriminator, self.classifier, self.source_dataset, self.target_dataset,
                              self.discriminator_optimizer, self.target_encoder_optimizer, self.num_iterations, self.batch_size, self.device)
         adapter.adapt()
-        target_tester = NetworkTester(self.target_encoder, self.classifier, self.target_dataset, self.batch_size, self.device)
+        print('Testing the accuracy of target encoder on the target domain after adaptation...')
         target_tester.test()
